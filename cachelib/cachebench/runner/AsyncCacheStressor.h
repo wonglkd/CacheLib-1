ac3d3e67bbce3c62407d961d074a5973504c02e7
Daniel Wong <wonglkd@gmail.com>
2023-02-27 21:15:06 -0500

    Use XDCHECK_NE
    Use XDCHECK_NE instead of XDCHECK to see if fixes build, revert use of std::next

--- cachelib/cachebench/runner/AsyncCacheStressor.h
+++ cachelib/cachebench/runner/AsyncCacheStressor.h
@@ -303,9 +303,9 @@ class AsyncCacheStressor : public Stressor {
       } else {
         wHdl = std::move(hdl).toWriteHandle();
       }
-      XDCHECK(std::next(req->sizeBegin) != req->sizeEnd);
+      XDCHECK_NE(req->sizeBegin + 1, req->sizeEnd);
       bool chainSuccessful = false;
-      for (auto j = std::next(req->sizeBegin); j != req->sizeEnd; j++) {
+      for (auto j = req->sizeBegin + 1; j != req->sizeEnd; j++) {
         ++stats.addChained;
 
         const auto size = *j;
