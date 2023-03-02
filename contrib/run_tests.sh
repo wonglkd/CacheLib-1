1a2b08396a25f21d9df49a94e8f53b4c68ab8ddd
Daniel Wong <wonglkd@gmail.com>
2023-03-02 16:45:06 -0500

    run_tests: improvements, timeout

--- contrib/run_tests.sh
+++ contrib/run_tests.sh
@@ -23,6 +23,9 @@ OPTIONAL+=("allocator-test-NvmCacheTests")
 OPTIONAL+=("navy-test-DeviceTest")
 OPTIONAL+=("shm-test-test_page_size")
 
+TEST_TIMEOUT=5m
+BENCHMARK_TIMEOUT=20m
+
 # Skip long-running benchmarks.
 TO_SKIP=()
 TO_SKIP+=("benchmark-test-CompactCacheBench")  # 26 mins.
@@ -41,80 +44,110 @@ PREFIX="$PWD/opt/cachelib/"
 LD_LIBRARY_PATH="$PREFIX/lib:$PREFIX/lib64:${LD_LIBRARY_PATH:-}"
 export LD_LIBRARY_PATH
 
-echo "::group::Running tests for CI"
+echo "Max test duration: $TEST_TIMEOUT"
+
+echo "::group::Running tests for CI (in parallel)"
 find * -type f -not -name "*bench*" -executable \
   | grep -vF "$TO_SKIP_LIST" \
-  | xargs -n1 -I {} make -s {}.log || echo Test {} failed
+  | awk ' { print $1 ".log" } ' | tr '\n' ' ' \
+  | xargs timeout --preserve-status $TEST_TIMEOUT make -j -s
+echo "::endgroup::"
 echo "Successful tests: `find -name '*.ok' | wc -l`"
 echo "Failed tests: `find -name '*.fail' | wc -l`"
-echo "::endgroup::"
 echo
-echo "::group::Running benchmarks for CI"
+echo "::group::Running benchmarks for CI (in parallel)"
 find * -type f -name "*bench*" -executable \
   | grep -vF "$TO_SKIP_LIST" \
-  | xargs -n1 -I {} make -s {}.log || echo Test {} failed
+  | awk ' { print $1 ".log" } ' | tr '\n' ' ' \
+  | xargs timeout --preserve-status $BENCHMARK_TIMEOUT make -j -s
+echo "::endgroup::"
 echo "Successful benchmarks: `find -name '*bench*.ok' | wc -l`"
 echo "Failed benchmarks: `find -name '*bench*.fail' | wc -l`"
-echo "::endgroup::"
 
-N_PASSED=`find -name '*.ok' | wc -l`
-N_FAILED=`find -name '*.fail' | wc -l`
-N_IGNORED=`find * -type f -name "*.fail" -exec basename {} .log.fail ';' | grep -F "$OPTIONAL_LIST" | wc -l`
+
+TESTS_PASSED=`find * -name '*.log.ok' | sed 's/\.[^.]*$//'`
+TESTS_FAILED=`find * -name '*.log.fail' | sed 's/\.[^.]*$//'`
+TESTS_TIMEOUT=`find * -type f -executable | grep -vF "$TESTS_PASSED\n$TESTS_FAILED" | sed 's/\.[^.]*$//'`
+TESTS_IGNORED=`echo $TESTS_FAILED | tr ' ' '\n' | grep -F "$OPTIONAL_LIST"`
+FAILURES_UNIGNORED=`echo $TESTS_FAILED | tr ' ' '\n' | grep -vF "$OPTIONAL_LIST"`
+
+N_TIMEOUT=${#TESTS_TIMEOUT[@]}
+N_PASSED=${#TESTS_PASSED[@]}
+N_FAILED=${#TESTS_FAILED[@]}
+N_IGNORED=${#TESTS_IGNORED[@]}
+N_FAILURES_UNIGNORED=${#FAILURES_UNIGNORED[@]}
 N_SKIPPED=${#TO_SKIP[@]}
-let "N_FAILED_NOT_IGNORED = $N_FAILED - $N_IGNORED"
+
 echo "## Test summary" >> $GITHUB_STEP_SUMMARY
-echo "| Passed | Failed | Ignored | Skipped" >> $GITHUB_STEP_SUMMARY
-echo "|--|--|--|--|" >> $GITHUB_STEP_SUMMARY
-echo "| $N_PASSED | $N_FAILED | $N_IGNORED | $N_SKIPPED |" >> $GITHUB_STEP_SUMMARY
+echo "| Passed | Failed | Ignored | Timeout | Skipped" >> $GITHUB_STEP_SUMMARY
+echo "|--------|--------|---------|---------|---------|" >> $GITHUB_STEP_SUMMARY
+echo "| $N_PASSED | $N_FAILED | $N_IGNORED | $N_TIMEOUT | $N_SKIPPED |" >> $GITHUB_STEP_SUMMARY
 
 
-if ls *.fail > /dev/null 2>&1; then
+if [ $N_FAILED -ne 0 ]; then
+    if [ $N_IGNORED -ne 0 ]; then
+        echo
+        echo "::group::Ignored test failures "
+        echo $TESTS_IGNORED | tr ' ' '\n'
+        echo "::endgroup"
+
+        echo >> $GITHUB_STEP_SUMMARY
+        echo "## Ignored test failures" >> $GITHUB_STEP_SUMMARY
+        echo $TESTS_IGNORED | awk -v RS=' ' ' { print "- " $1 } ' >> $GITHUB_STEP_SUMMARY
+    fi 
+
+    if [ $N_FAILURES_UNIGNORED -eq 0 ]; then
+        STATUS=0
+        echo "Only ignored tests failed."
+    else
+        STATUS=1
+        echo
+        echo "::group::Failing tests"
+        echo $FAILURES_UNIGNORED | tr ' ' '\n'
+        echo "::endgroup"
+
+        echo >> $GITHUB_STEP_SUMMARY
+        echo "## Failing tests" >> $GITHUB_STEP_SUMMARY
+        echo $FAILURES_UNIGNORED | awk -v RS=' ' ' { print "- " $1 } ' >> $GITHUB_STEP_SUMMARY
+
+        echo "::warning $N_FAILURES_UNIGNORED tests/benchmarks failed."
+    fi
+
+    echo
     echo "::group::Failure details"
     grep "Segmentation fault" *.log || true
     grep "FAILED.*ms" *.log || true
     echo "::endgroup"
-    echo
-    if [ $N_IGNORED -ne 0 ]; then
-        echo "::group::Ignored test failures "
-        find * -type f -name "*.fail" -exec basename {} .log.fail ';' \
-            | grep -F "$OPTIONAL_LIST"
-        echo "::endgroup"
-        echo
-        echo >> $GITHUB_STEP_SUMMARY
-        echo "## Ignored test failures" >> $GITHUB_STEP_SUMMARY
-        find * -type f -name "*.fail" -exec basename {} .log.fail ';' \
-            | grep -F "$OPTIONAL_LIST" \
-            | awk ' { print "- " $1 } ' >> $GITHUB_STEP_SUMMARY
-    fi 
-    echo "::group::Summary of failures"
-    find * -type f -name "*.fail" -exec basename {} .log.fail ';' \
-        | grep -vF "$OPTIONAL_LIST"
-    STATUS=$?
-    echo "::endgroup"
-    echo
 
-    if [ $STATUS -ne 0 ]; then
-        echo "Only ignored tests failed."
-    else
-        echo >> $GITHUB_STEP_SUMMARY
-        echo "## Failing tests" >> $GITHUB_STEP_SUMMARY
-        find * -type f -name "*.fail" -exec basename {} .log.fail ';' \
-            | grep -vF "$OPTIONAL_LIST" \
-            | awk ' { print "- " $1 } ' >> $GITHUB_STEP_SUMMARY
-
-        echo "::warning $N_FAILED_NOT_IGNORED tests/benchmarks failed."
-    fi
-
+    echo >> $GITHUB_STEP_SUMMARY
     echo "## Failure details" >> $GITHUB_STEP_SUMMARY
     echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
     grep "Segmentation fault" *.log || true >> $GITHUB_STEP_SUMMARY
     grep "FAILED.*ms" *.log || true >> $GITHUB_STEP_SUMMARY
     echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
 
-    # Comment out for now so we can figure out which tests work on which
-    # if [ $STATUS -eq 0 ]; then
-    #     exit 1
-    # fi
 else
+    STATUS=0
+    echo
     echo "All tests passed."
+fi
+
+if [ $N_TIMEOUT -ne 0 ]; then
+    echo
+    echo "::group::Timed out tests"
+    echo $TESTS_TIMEOUT | tr ' ' '\n'
+    echo "::endgroup"
+
+
+    echo "## Tests timed out" >> $GITHUB_STEP_SUMMARY
+    echo $TESTS_TIMEOUT | awk -v RS=' ' ' { print "- " $1 } ' >> $GITHUB_STEP_SUMMARY
+
+fi
+
+
+if [ $STATUS -eq 0 ]; then
+    echo
+    echo "Return as error"
+    # Comment out for now so we can figure out which tests work on which
+    # exit 1
 fi
