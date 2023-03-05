55d658d888b7b84f6b6cb7f7ceb1e19fbcb07c8c
Daniel Wong <wonglkd@gmail.com>
2023-03-05 01:35:41 -0500

    Update run_tests.sh

--- contrib/run_tests.sh
+++ contrib/run_tests.sh
@@ -22,7 +22,11 @@ fi
 # Optional (e.g., flaky tests).
 OPTIONAL=()
 # Failed on CI
+
+allocator-test-AllocatorTypeTest
 OPTIONAL+=("allocator-test-AllocationClassTest")  # Ubuntu 18 (segfault)
+OPTIONAL+=("allocator-test-AllocatorResizeTypeTest")  # Rocky 8.6
+OPTIONAL+=("allocator-test-AllocatorTypeTest")  # CentOS 8.5, Rocky 8.6
 OPTIONAL+=("allocator-test-MemoryAllocatorTest")  # Ubuntu 18 (segfault)
 OPTIONAL+=("allocator-test-MM2QTest")  # Ubuntu 18 (segfault)
 OPTIONAL+=("allocator-test-BlockCacheTest")  # Rocky 8.6
